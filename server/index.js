9d7a0ed77c01ab968f275f35752ea7f030e1986a
Janhvi Pandey <shivipandey993@gmail.com>
2025-06-01 15:51:06 +0530

    cors origin changes and start script included

--- server/index.js
+++ server/index.js
@@ -16,7 +16,14 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://quick-cart-8wr4.vercel.app/"
+  ],
+  credentials: true,
+}));
+
 app.use(bodyParser.json());
 
 // Routes
