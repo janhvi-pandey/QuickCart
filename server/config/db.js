fd1fda4ae3bb1fec578033b06b6a7423acd87b64
Janhvi Pandey <shivipandey993@gmail.com>
2025-06-01 16:24:34 +0530

    Db file changed

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,8 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
+    
     console.log('MongoDB connected 😊');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
